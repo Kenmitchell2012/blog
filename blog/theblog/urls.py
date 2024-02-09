81f4765543b439018a685471884de2e99f00f545
Kendrick Mitchell <39783561+Kenmitchell2012@users.noreply.github.com>
2024-02-09 02:39:44 +0000

    added addpost page

--- blog/theblog/urls.py
+++ blog/theblog/urls.py
@@ -1,8 +1,9 @@
-from . views import HomeView, View_Post
+from . views import HomeView, View_Post, CreateView, AddPost
 from django.urls import path
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),
     path('post/<int:pk>', View_Post.as_view(), name='post'),
+    path('new_post/', AddPost.as_view(), name='new_post'),
 ]
--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from . models import Post
 
 # Create your views here.
@@ -16,3 +16,8 @@ class HomeView(ListView):
 class View_Post(DetailView):
     model = Post
     template_name = 'view.html'
+
+class AddPost(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
