54f4da9baf82f5e69d1cf68545a570be93f3895b
Kendrick Mitchell <39783561+Kenmitchell2012@users.noreply.github.com>
2024-02-12 19:46:56 +0000

    created edit post page

--- blog/theblog/urls.py
+++ blog/theblog/urls.py
@@ -1,4 +1,4 @@
-from . views import HomeView, View_Post, CreateView, AddPost
+from . views import HomeView, View_Post, CreateView, AddPost, UpdatePost
 from django.urls import path
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('post/<int:pk>', View_Post.as_view(), name='post'),
     path('new_post/', AddPost.as_view(), name='new_post'),
+    path('update_post/<int:pk>', UpdatePost.as_view(), name='update_post'),
 ]
--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from . models import Post
 
 # Create your views here.
@@ -21,3 +21,8 @@ class AddPost(CreateView):
     model = Post
     template_name = 'add_post.html'
     fields = '__all__'
+
+class UpdatePost(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['title', 'title_tag', 'body']
