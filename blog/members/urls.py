0c9631659816fab52b108776a901040320c54c90
Kendrick Mitchell <kenmitchell2012@icloud.com>
2024-03-01 18:49:57 -0600

    fixed change password page

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -2,14 +2,14 @@ from django.contrib.auth.views import LogoutView
 from . views import UserRegisterView, UserEditView, PasswordsChangeView
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from. import views
+from . import views
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
-    path('password/', PasswordsChangeView.as_view(),name='password'),
-    # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password'),
+    path('<int:uid>/password/', PasswordsChangeView.as_view(),name='password'),
+    path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password'),
     path('password_success/', views.password_success, name='password_success'),
 ]
--- blog/theblog/urls.py
+++ blog/theblog/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('update_post/<int:pk>', UpdatePost.as_view(), name='update_post'),
     path('delete_post/<int:pk>', DeletePost.as_view(), name='delete_post'),
-    #path for liked posts
+    # path for liked posts
     path('like/<int:pk>', LikeView, name='like_post'),
+
     path('category/<str:cats>', CategoryView, name='category'),
     path('category_list/', CategoryListView, name='category_list'),
     path('<int:uid>/', include('members.urls'))
