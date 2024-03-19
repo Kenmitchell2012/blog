8189cdb21727863aa222bbd4fd9d45df31dbd590
Kendrick Mitchell <kenmitchell2012@icloud.com>
2024-03-18 20:54:45 -0500

    creating profile page needs work

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LogoutView
-from . views import UserRegisterView, UserEditView, PasswordsChangeView
+from . views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -13,4 +13,6 @@ urlpatterns = [
     path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password'),
     # path('<int:uid>/password/', PasswordsChangeView.as_view(template_name='registration/password_change.html'), name='password'),
     path('password_success/', views.password_success, name='password_success'),
+    # profile page
+    path('<int:pk>/', ShowProfilePageView.as_view(), name='show_profile_page'),
 ]
--- blog/members/views.py
+++ blog/members/views.py
@@ -1,13 +1,33 @@
 from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
-from django.shortcuts import render
-from  django.views import generic
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from . forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from django.views.generic import DetailView
+from theblog.models import Profile
 
 # Create your views here.
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        context["users"] = users
+        return context
+    
+
+
+
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name ='registration/register.html'
